4b2bdaaaa4fb6c668eaeb8f098340882026479d5
isal763 <faisaladititisno@gmail.com>
2021-10-25 14:58:22 +0700

    menambahkan file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# latihanpython01
+# Latihanpython01
+## Latihan input dan output data pada python
+### cara menginputnya
+<p>contoh : nama=input("siapa nama kamu:")
+
+### cara mengoutput 
+<p>contoh: print("hello",nama,"umur kamu adalah",umur,"tahun")
+
+## contoh gambar input dan output yang saya buat 
+### input 
+![gambar1](ss/ssinput.png)
+### output
+![gambar2](ss/ssoutput.png)
+
--- latihan01.py
+++ latihan01.py
@@ -1,12 +1,13 @@
-nama = input("jadi nama anda adalah:")
-namap = input("dan biasa dipanggil:")
-ttl = input("anda lahir di:")
-umur = input("saat ini umur anda:")
+nama            = input("jadi nama anda adalah:")
+namap           = input("dan biasa dipanggil:")
+ttl             = input("anda lahir di:")
+umur            = input("saat ini umur anda:")
 namauniversitas = input("anda saat ini mahasiswa di:")
-nim = ("dengan nomor Nim:")
-alamat = input("dan anda tinggal:")
-no = input("Nomor Telepon anda adalah:")
+nim             = input("dengan nomor Nim:")
+alamat          = input("dan anda tinggal:")
+no              = input("Nomor Telepon anda adalah:")
 print("Jadi nama anda adalah", nama, "dan biasa di panggil", namap,
       "anda lahir di", ttl, "saat ini umur anda", umur, "anda saat ini mahasiswa di ", namauniversitas,
-      "dengan nomor Nim ", nim,
+      "berapa no nim anda", nim,
       "dan anda tinggal", alamat, "Nomor Telepon anda adalah", no)
+
